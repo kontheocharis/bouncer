c4a0d1e5a8289598761298f62a92b7153d843a07
Constantine Theocharis <kontheocharis@gmail.com>
2019-10-17 00:00:54 +0200

    Start implementing Disney BRDF function

--- src/Tracer.cc
+++ src/Tracer.cc
@@ -3,12 +3,45 @@
 #include "Geometry.hh"
 #include "Material.hh"
 
+#include <cmath>
+
 namespace bcr
 {
 
 ColorRGB Tracer::trace_ray(const Ray3& ray) const
 {
-    auto sheen = this->calculate_sheen();
+    return ColorRGB::white();
+}
+
+ColorRGB Tracer::compute_brdf(
+        const Vec3& view_vector,
+        const Vec3& light_vector,
+        const Vec3& normal_vector,
+        const Material& mat) const
+{
+    Tracer::BrdfParameters params;
+
+    params.view_vector = view_vector;
+    params.light_vector = light_vector;
+    params.half_vector = (view_vector + light_vector).normalized();
+    params.normal_vector = normal_vector;
+
+    params.cos_theta_view = 
+        view_vector.dot(normal_vector) / (view_vector.norm() * normal_vector.norm());
+
+    params.cos_theta_light = 
+        light_vector.dot(normal_vector) / (light_vector.norm() * normal_vector.norm());
+
+    params.cos_theta_h = 
+        normal_vector.dot(params.half_vector) / (normal_vector.norm() * params.half_vector.norm());
+
+    params.cos_theta_d = 
+        view_vector.dot(params.half_vector) / (view_vector.norm() * params.half_vector.norm());
+
+    auto diffuse = this->calculate_diffuse(params, mat);
+    auto specular = this->calculate_specular(params, mat);
+    
+    return diffuse + specular;
 }
 
 ColorRGB Tracer::calculate_albedo_tint(const ColorRGB& color) const
@@ -16,6 +49,14 @@ ColorRGB Tracer::calculate_albedo_tint(const ColorRGB& color) const
     // CIELAB conversions and removal of lightness
     double luminance = ColorRGB {0.3, 0.6, 1.0}.dot(color);
     return (luminance > 0.0) ? color / luminance : ColorRGB::white();
+}
+
+ColorRGB Tracer::calculate_diffuse(const Tracer::BrdfParameters& params, const Material& mat) const
+{
+    auto fresnel = 0.5 + 2 * params.cos_theta_d * params.cos_theta_d * mat.roughness;
+    return (mat.albedo / M_PI) 
+        * (1 + (fresnel - 1) * pow(1 - params.cos_theta_light, 5)) 
+        * (1 + (fresnel - 1) * pow(1 - params.cos_theta_view, 5));
 }
 
 } /* bcr */ 
--- src/Tracer.hh
+++ src/Tracer.hh
@@ -11,6 +11,7 @@
 namespace bcr
 {
 
+// Implements Disney's Principled BRDF
 class Tracer
 {
 public:
@@ -19,12 +20,38 @@ public:
     std::vector<ColorRGB> capture() const;
 
 private:
+    struct BrdfParameters
+    {
+        Vec3 view_vector;
+        Vec3 light_vector;
+
+        Vec3 half_vector;
+        Vec3 normal_vector;
+
+        double cos_theta_view; // Angle of incidence of view vector
+        double cos_theta_light; // Angle of incidence of light vector
+        double cos_theta_h; // Angle between normal vector and half-vector
+        double cos_theta_d; // Angle between light vector (or view vector) and half-vector
+    };
+
     const Scene& m_scene;
     const Camera& m_camera;
 
     ColorRGB trace_ray(const Ray3&) const;
+
+    ColorRGB compute_brdf(
+            const Vec3& view_vector,
+            const Vec3& light_vector,
+            const Vec3& normal_vector,
+            const Material& material) const;
+
     ColorRGB calculate_albedo_tint(const ColorRGB& color) const;
-    ColorRGB calculate_sheen() const;
+
+    ColorRGB calculate_diffuse(const BrdfParameters&, const Material&) const;
+
+    ColorRGB calculate_specular(const BrdfParameters&, const Material&) const;
+
+    ColorRGB calculate_sheen(const BrdfParameters&) const;
 };
 
 } /* bcr */
